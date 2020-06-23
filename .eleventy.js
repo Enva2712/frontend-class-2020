8df8b94c16cbcb24bcb083093b540d257f6f8179
Evan Rusmisel <evanrusmisel@gmail.com>
2020-06-22 20:24:33 -0500

    Sort lessons collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(config) {
+    config.addCollection('lessons', collections =>
+        collections.getFilteredByTag('lesson')
+        .sort((a, b) => a.data.lesson - b.data.lesson)
+    )
+
 	return {
 		dir: {
 			input: 'content',
