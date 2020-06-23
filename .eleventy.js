0a955bb551f0816d3ce1ea2e14bfc19f10689d41
Evan Rusmisel <evanrusmisel@gmail.com>
2020-06-23 12:15:14 -0500

    fix start:css npm script

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+
 module.exports = function(config) {
     config.addCollection('lessons', collections =>
         collections.getFilteredByTag('lesson')
         .sort((a, b) => a.data.lesson - b.data.lesson)
     )
+
+    config.addPlugin(syntaxHighlight)
 
 	return {
 		dir: {
